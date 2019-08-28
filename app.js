25ec69e40407780a57071a495b94dc263635a120
Pablo Escrivá <pabloescrivagallardo@gmail.com>
2019-08-28 12:50:23 +0200

    error test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const createError = require('http-errors');
+123const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
